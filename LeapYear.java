55029eb0e8486f8e8b5fe8c3d0b05012e32dcae4
kirankasipeta <kirankasipeta>
2023-11-03 13:07:21 +0530

    updated test file

--- LeapYear.java
+++ LeapYear.java
@@ -2,7 +2,7 @@ class LeapYear
 {
 	public static void main(String[]args)
 	{
-		int year=2001;
+		int year=2000;
 		if((year%4==0&&year%100!=0)||(year%400==0))
 			System.out.println("leap year");
 		else
